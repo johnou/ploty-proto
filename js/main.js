ff51b4999041daaeebae533510736196e8146b35
Johno Crawford <johno@sulake.com>
2017-02-20 12:18:20 +0100

    Formatting.

--- js/main.js
+++ js/main.js
@@ -1,122 +1,122 @@
-Plotly.d3.csv('https://raw.githubusercontent.com/johnou/ploty-proto/master/netty-jmh-results-handshake.csv', function(rows){
+Plotly.d3.csv('https://raw.githubusercontent.com/johnou/ploty-proto/master/netty-jmh-results-handshake.csv', function (rows) {
 
-    var openSslRows = rows.filter(function(row){
-      return row['Param: sslProvider'] == 'OPENSSL';
-    });
-      
-    var openSslTrace = {
-      name: 'OPENSSL',
-      type: 'bar',
-      x: openSslRows.map(function(row){
-        return [row['Param: bufferType'], ' ', row['Param: cipher']].join('');
-      }),
-      y: openSslRows.map(function(row){
-        return row['Score'];
-      })
-    };
+  var openSslRows = rows.filter(function (row) {
+    return row['Param: sslProvider'] == 'OPENSSL';
+  });
 
-    var jdkRows = rows.filter(function(row){
-      return row['Param: sslProvider'] == 'JDK';
-    });
-    
-    var jdkTrace = {
-      name: 'JDK',
-      type: 'bar',
-      x: jdkRows.map(function(row){
-        return [row['Param: bufferType'], ' ', row['Param: cipher']].join('');
-      }),
-      y: jdkRows.map(function(row){
-        return row['Score'];
-      })
-    };
-    
-    var layout = {
-      title: 'SslEngine handshake JMH benchmark',
-      barmode: 'group',
-      yaxis: {title: "Handshake throughput score ops/s"},
-    };
+  var openSslTrace = {
+    name: 'OPENSSL',
+    type: 'bar',
+    x: openSslRows.map(function (row) {
+      return [row['Param: bufferType'], ' ', row['Param: cipher']].join('');
+    }),
+    y: openSslRows.map(function (row) {
+      return row['Score'];
+    })
+  };
 
-    Plotly.plot(document.getElementById('handshake'), [openSslTrace, jdkTrace], layout, {showLink: false});
+  var jdkRows = rows.filter(function (row) {
+    return row['Param: sslProvider'] == 'JDK';
+  });
+
+  var jdkTrace = {
+    name: 'JDK',
+    type: 'bar',
+    x: jdkRows.map(function (row) {
+      return [row['Param: bufferType'], ' ', row['Param: cipher']].join('');
+    }),
+    y: jdkRows.map(function (row) {
+      return row['Score'];
+    })
+  };
+
+  var layout = {
+    title: 'SslEngine handshake JMH benchmark',
+    barmode: 'group',
+    yaxis: {title: "Handshake throughput score ops/s"},
+  };
+
+  Plotly.plot(document.getElementById('handshake'), [openSslTrace, jdkTrace], layout, {showLink: false});
 });
 
-Plotly.d3.csv('https://raw.githubusercontent.com/johnou/ploty-proto/master/netty-jmh-results-wrap.csv', function(rows){
+Plotly.d3.csv('https://raw.githubusercontent.com/johnou/ploty-proto/master/netty-jmh-results-wrap.csv', function (rows) {
 
-    var openSslRows = rows.filter(function(row){
-      return row['Param: sslProvider'] == 'OPENSSL';
-    });
+  var openSslRows = rows.filter(function (row) {
+    return row['Param: sslProvider'] == 'OPENSSL';
+  });
 
-    var openSslTrace = {
-      name: 'OPENSSL',
-      type: 'bar',
-      x: openSslRows.map(function(row){
-        return [row['Param: bufferType'], ' ', row['Param: messageSize'], ' ', row['Param: cipher']].join('');
-      }),
-      y: openSslRows.map(function(row){
-        return row['Score'];
-      })
-    };
-    
-    var jdkRows = rows.filter(function(row){
-      return row['Param: sslProvider'] == 'JDK';
-    });
+  var openSslTrace = {
+    name: 'OPENSSL',
+    type: 'bar',
+    x: openSslRows.map(function (row) {
+      return [row['Param: bufferType'], ' ', row['Param: messageSize'], ' ', row['Param: cipher']].join('');
+    }),
+    y: openSslRows.map(function (row) {
+      return row['Score'];
+    })
+  };
 
-    var jdkTrace = {
-      name: 'JDK',
-      type: 'bar',
-      x: jdkRows.map(function(row){
-        return [row['Param: bufferType'], ' ', row['Param: messageSize'], ' ', row['Param: cipher']].join('');
-      }),
-      y: jdkRows.map(function(row){
-        return row['Score'];
-      })
-    };
+  var jdkRows = rows.filter(function (row) {
+    return row['Param: sslProvider'] == 'JDK';
+  });
 
-    var layout = {
-      title: 'SslEngine wrap JMH benchmark',
-      barmode: 'group',
-      yaxis: {title: "Wrap throughput score ops/s"},
-    };
+  var jdkTrace = {
+    name: 'JDK',
+    type: 'bar',
+    x: jdkRows.map(function (row) {
+      return [row['Param: bufferType'], ' ', row['Param: messageSize'], ' ', row['Param: cipher']].join('');
+    }),
+    y: jdkRows.map(function (row) {
+      return row['Score'];
+    })
+  };
 
-    Plotly.plot(document.getElementById('wrap'), [openSslTrace, jdkTrace], layout, {showLink: false});
+  var layout = {
+    title: 'SslEngine wrap JMH benchmark',
+    barmode: 'group',
+    yaxis: {title: "Wrap throughput score ops/s"},
+  };
+
+  Plotly.plot(document.getElementById('wrap'), [openSslTrace, jdkTrace], layout, {showLink: false});
 });
 
-Plotly.d3.csv('https://raw.githubusercontent.com/johnou/ploty-proto/master/netty-jmh-results-unwrap.csv', function(rows){
+Plotly.d3.csv('https://raw.githubusercontent.com/johnou/ploty-proto/master/netty-jmh-results-unwrap.csv', function (rows) {
 
-    var openSslRows = rows.filter(function(row){
-      return row['Param: sslProvider'] == 'OPENSSL';
-    });
+  var openSslRows = rows.filter(function (row) {
+    return row['Param: sslProvider'] == 'OPENSSL';
+  });
 
-    var openSslTrace = {
-      name: 'OPENSSL',
-      type: 'bar',
-      x: openSslRows.map(function(row){
-        return [row['Param: bufferType'], ' ', row['Param: messageSize'], ' ', row['Param: cipher']].join('');
-      }),
-      y: openSslRows.map(function(row){
-        return row['Score'];
-      })
-    };
-    
-    var jdkRows = rows.filter(function(row){
-      return row['Param: sslProvider'] == 'JDK';
-    });
+  var openSslTrace = {
+    name: 'OPENSSL',
+    type: 'bar',
+    x: openSslRows.map(function (row) {
+      return [row['Param: bufferType'], ' ', row['Param: messageSize'], ' ', row['Param: cipher']].join('');
+    }),
+    y: openSslRows.map(function (row) {
+      return row['Score'];
+    })
+  };
 
-    var jdkTrace = {
-      name: 'JDK',
-      type: 'bar',
-      x: jdkRows.map(function(row){
-        return [row['Param: bufferType'], ' ', row['Param: messageSize'], ' ', row['Param: cipher']].join('');
-      }),
-      y: jdkRows.map(function(row){
-        return row['Score'];
-      })
-    };
+  var jdkRows = rows.filter(function (row) {
+    return row['Param: sslProvider'] == 'JDK';
+  });
 
-    var layout = {
-      title: 'SslEngine unwrap JMH benchmark',
-      barmode: 'group',
-      yaxis: {title: "Unwrap throughput score ops/s"},
-    };
+  var jdkTrace = {
+    name: 'JDK',
+    type: 'bar',
+    x: jdkRows.map(function (row) {
+      return [row['Param: bufferType'], ' ', row['Param: messageSize'], ' ', row['Param: cipher']].join('');
+    }),
+    y: jdkRows.map(function (row) {
+      return row['Score'];
+    })
+  };
 
-    Plotly.plot(document.getElementById('unwrap'), [openSslTrace, jdkTrace], layout, {showLink: false});
+  var layout = {
+    title: 'SslEngine unwrap JMH benchmark',
+    barmode: 'group',
+    yaxis: {title: "Unwrap throughput score ops/s"},
+  };
+
+  Plotly.plot(document.getElementById('unwrap'), [openSslTrace, jdkTrace], layout, {showLink: false});
 });
